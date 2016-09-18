1420299e90fbf2a99264cd0c3b3da55b90903371
saiannapurna <saiannapurna>
2016-09-18 17:50:51 +0530

    a line of code added to main.java in remote branch

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ class Main
 		System.out.println("Hello Satish");
 		System.out.println("This is from remote Repo");
 		System.out.println("Hello Developer branch");
+		System.out.println("Hello Remote branch");
 	}
 }
